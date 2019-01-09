610f571fccd548e5d4bb264cff147bf78858fb01
Sujan Chakma <34137507+SujanChakma@users.noreply.github.com>
2019-01-09 20:57:19 +0600

    Add files via upload

---
+++ CalculatorProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatorproject;
+
+/**
+ *
+ * @author csl tb
+ */
+public class CalculatorProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CalculatorDesign C = new CalculatorDesign();
+        C.setVisible(true);
+    }
+}
